8427b12803a1ce792fba49a938cf1196f96a4add
Josemigg16 <soldado159753@gmail.com>
2024-02-18 18:44:30 -0400

    send a object to localstoage instead a string

--- astro-cortito/src/components/ShowState.jsx
+++ astro-cortito/src/components/ShowState.jsx
@@ -4,14 +4,14 @@ import { useStore } from "@nanostores/react"
 const ShowState = () => {
 
     const state = useStore($shortcut)
-    console.log($shortcut.value)
+    console.log(state)
     const shortcut = `${window.location.origin}/${state}`
 
     return (
         <div
             className="relative h-12 w-full min-w-[200px] flex items-center border-2 border-gray-500 rounded-md pl-4"
         >
-            <p>{shortcut || ""}</p>
+            <p>{state ? shortcut : "An error has been ocurred"}</p>
         </div>
     )
 }
--- astro-cortito/src/shortcutstore.js
+++ astro-cortito/src/shortcutstore.js
@@ -3,12 +3,12 @@ export const $shortcut = atom("")
 if (typeof window !== "undefined") {
     const state = JSON.parse(window.localStorage.getItem("shortcut"))
     if (state) {
-        $shortcut.set(state)
+        $shortcut.set(state.shortcut)
     }
 }
 
 export const updateShortcut = (newState) => {
-    window.localStorage.setItem("shortcut", JSON.stringify(newState));
+    window.localStorage.setItem("shortcut", JSON.stringify({shortcut: newState}));
     $shortcut.set(newState)
     console.log($shortcut.value)
 }
