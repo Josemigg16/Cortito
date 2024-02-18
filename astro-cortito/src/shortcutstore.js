822fbfd08eac36584de4d682a40ebb8fefcdce13
Josemigg16 <soldado159753@gmail.com>
2024-02-18 17:34:29 -0400

    rm localstorage

--- astro-cortito/src/shortcutstore.js
+++ astro-cortito/src/shortcutstore.js
@@ -1,6 +1,5 @@
 import { atom } from "nanostores"
-export const $shortcut = atom(
-    typeof window !== "undefined" ? JSON.parse(typeof window.localStorage.getItem("shortcut") !== "undefined" ? window.localStorage.getItem("shortcut") : "") : "")
+export const $shortcut = atom("")
 export const updateShortcut = (newState) => {
     window.localStorage.setItem("shortcut", JSON.stringify(newState));
     $shortcut.set(newState)
