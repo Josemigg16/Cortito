139a2587a32f3179cd755fd2d34a9db2b51968f2
Josemigg16 <soldado159753@gmail.com>
2024-02-19 09:53:58 -0400

    make NoUser model

--- astro-cortito/src/components/ShowState.jsx
+++ astro-cortito/src/components/ShowState.jsx
@@ -4,12 +4,13 @@ import { useStore } from "@nanostores/react"
 const ShowState = () => {
 
     const state = useStore($shortcut)
+    const lastShortcut = state[state.length - 1]
     console.log(state)
-    const shortcut = `${window.location.origin}/${state}`
+    const shortcut = `${window.location.origin}/${lastShortcut}`
 
     return (
         <div
-            className="relative h-12 w-full min-w-[200px] flex items-center border-2 border-gray-500 rounded-md pl-4"
+            className="relative h-12 w-full min-w-[200px] flex items-center border-2 border-gray-500 rounded-md pl-4" tabIndex={0}
         >
             <p>{state ? shortcut : "An error has been ocurred"}</p>
         </div>
--- astro-cortito/src/shortcutstore.js
+++ astro-cortito/src/shortcutstore.js
@@ -1,20 +1,19 @@
 import { atom } from "nanostores"
-export const $shortcut = atom("")
+export const $shortcut = atom([])
 if (typeof window !== "undefined") {
     try {
-        const state = JSON.parse(window.localStorage.getItem("shortcut"))
-        $shortcut.set(state.shortcut)
+        const state = JSON.parse(window.localStorage.getItem("shortcut")) || []
+        $shortcut.set(state)
     } catch {
-        $shortcut.set("")
+        $shortcut.set([])
     }
 }
 
 export const updateShortcut = (newState) => {
-    window.localStorage.setItem("shortcut", JSON.stringify({ shortcut: newState }));
-    $shortcut.set(newState)
-    console.log($shortcut.value)
+    window.localStorage.setItem("shortcut", JSON.stringify([...$shortcut.get(), newState]));
+    $shortcut.set([...$shortcut.get(), newState])
 }
 
 export const checkState = () => {
-    console.log($shortcut.value)
+    console.log($shortcut.get())
 }
