0a5365b2c3d92e74a2442fbc7385b4854914372b
Josemigg16 <soldado159753@gmail.com>
2024-02-19 10:09:45 -0400

    fix state

--- astro-cortito/src/shortcutstore.js
+++ astro-cortito/src/shortcutstore.js
@@ -1,17 +1,17 @@
 import { atom } from "nanostores"
-export const $shortcut = atom([])
+export const $shortcut = atom("")
 if (typeof window !== "undefined") {
     try {
         const state = JSON.parse(window.localStorage.getItem("shortcut")) || []
         $shortcut.set(state)
     } catch {
-        $shortcut.set([])
+        $shortcut.set("")
     }
 }
 
 export const updateShortcut = (newState) => {
-    window.localStorage.setItem("shortcut", JSON.stringify([...$shortcut.get(), newState]));
-    $shortcut.set([...$shortcut.get(), newState])
+    window.localStorage.setItem("shortcut", JSON.stringify(newState));
+    $shortcut.set(newState)
 }
 
 export const checkState = () => {
