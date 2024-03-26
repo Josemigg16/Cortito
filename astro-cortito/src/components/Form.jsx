d25f259a160e7fddfab16875717c86d20b98f63a
Jose Miguel <josemiggomez16@gmail.com>
2024-03-26 17:43:46 -0400

    autocomplete off

--- astro-cortito/src/components/Form.jsx
+++ astro-cortito/src/components/Form.jsx
@@ -39,6 +39,7 @@ const Form = ({ session }) => {
                     <input className="peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent selected:bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-indigo-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                         onFocus={() => { setBluring(false) }}
                         onBlur={() => { setBluring(true) }}
+                        autoComplete="off"
                         id="input"
                         type="text"
                         placeholder=" "
