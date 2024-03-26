521b8926533dfd2a49caa0da41f09f6315b82565
Jose Miguel <josemiggomez16@gmail.com>
2024-03-26 14:51:29 -0400

    fix port

--- backend-cortito/src/index.js
+++ backend-cortito/src/index.js
@@ -1,2 +1,3 @@
 import app from "./app.js";
-app.listen(3001, () => console.log("Server started on port", 3001));
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log("Server started on port", port));
