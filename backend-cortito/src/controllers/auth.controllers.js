a83aab069d0d9828bd175eb3d4a9bf96e047bacf
Jose Miguel <josemiggomez16@gmail.com>
2024-03-27 20:38:04 -0400

    remove fixedposts residual

--- backend-cortito/src/controllers/auth.controllers.js
+++ backend-cortito/src/controllers/auth.controllers.js
@@ -25,7 +25,7 @@ export const register = async (req, res) => {
                 email,
                 name,
                 posts: {
-                    connect: posts && fixedPosts
+                    connect: posts
                 }
 
             }
