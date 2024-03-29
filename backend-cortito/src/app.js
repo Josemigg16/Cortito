929e04432476ed18531517bccd46b25c8d73cba4
Jose Miguel <josemiggomez16@gmail.com>
2024-03-29 00:24:39 -0400

    delete security system: verify from headers

--- backend-cortito/src/app.js
+++ backend-cortito/src/app.js
@@ -9,10 +9,6 @@ const app = express()
 app.use(cors({
     origin: process.env.FRONTEND_URL,
 }))
-app.use((req, res, next) => {
-    if (req.headers.origin !== process.env.FRONTEND_URL) res.sendStatus(403)
-    else next()
-})
 app.use(morgan('dev'))
 app.use(express.json())
 
