083ac87821ca67014ed36b0099dce36acb980ba7
Jose Miguel <josemiggomez16@gmail.com>
2024-03-29 00:24:07 -0400

    delete console.log

--- astro-cortito/src/libs/getOneShortcut.js
+++ astro-cortito/src/libs/getOneShortcut.js
@@ -2,7 +2,6 @@ export default async function getOneShortcut(newLink) {
     try {
         const response = await fetch(`${import.meta.env.PUBLIC_API_URL}/shortcuts/${newLink}`)
         const shortcut = await response.json()
-        console.log(shortcut)
         return shortcut
     } catch (error) {
         console.log(error)
