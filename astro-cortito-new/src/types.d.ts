1ec51ff5f0efc570638dd044f7f9c264a65e9259
Jose Miguel <josemiggomez16@gmail.com>
2024-04-04 11:54:10 -0400

    moved types

---
+++ astro-cortito-new/src/types.d.ts
@@ -0,0 +1,11 @@
+export interface SVGProps {
+  className?: string
+}
+
+export interface Shortcut {
+  title?: string
+  description?: string
+  oldLink: string
+  newLink: string
+  authorID?: string
+}
