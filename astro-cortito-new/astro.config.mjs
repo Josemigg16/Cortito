421930a01ec4b24363c556b70c0f9dd05c8fdedc
Jose Miguel <josemiggomez16@gmail.com>
2024-04-04 09:44:53 -0400

    add auth-astro

--- astro-cortito-new/astro.config.mjs
+++ astro-cortito-new/astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import auth from "auth-astro";
 
-import react from '@astrojs/react'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-})
+  integrations: [tailwind(), react(), auth()],
+  output: 'server',
+  adapter: vercel()
+});
