06a223b8c3a44d08210eba0333c77b8d21bd5d57
Jose Miguel <josemiggomez16@gmail.com>
2024-04-04 11:54:59 -0400

    add slug for the shortcuts work

---
+++ astro-cortito-new/src/helpers/fixURL.ts
@@ -0,0 +1,7 @@
+export function fixURL(url: string): string {
+  if (url.includes('http://') || url.includes('https://')) {
+    return url
+  } else {
+    return 'http://' + url
+  }
+}
---
+++ astro-cortito-new/src/libs/getShortcut.ts
@@ -0,0 +1,16 @@
+import type { Shortcut } from '../types'
+
+export async function getShortcut(
+  path: string | undefined
+): Promise<Shortcut | undefined> {
+  try {
+    const response = await fetch(
+      `${import.meta.env.PUBLIC_API_URL}/shortcuts/${path}`
+    )
+    const shortcut = (await response.json()) as Shortcut
+    return shortcut
+  } catch (error) {
+    console.log(error)
+    return undefined
+  }
+}
