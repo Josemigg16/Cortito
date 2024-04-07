5d12c0aa559c5a18e828e66d1cfe22b72aa5c40f
Jose Miguel <josemiggomez16@gmail.com>
2024-04-07 16:17:08 -0400

    def types

--- astro-cortito-new/src/types.d.ts
+++ astro-cortito-new/src/types.d.ts
@@ -1,16 +1,29 @@
 export interface SVGProps {
-  className?: string
+	className?: string
 }
 
 export interface Shortcut {
-  id: string
-  title?: string
-  description?: string
-  oldLink: string
-  newLink: string
-  authorId?: string
+	id: string
+	title?: string
+	description?: string
+	oldLink: string
+	newLink: string
+	authorId?: string
+}
+export interface ShortcutWithEmail extends Shortcut {
+	id?: string
+	oldLink: string | undefined
+	newLink?: string
+	email?: string | null | undefined
 }
 
 export interface ShortcutAsProp {
-  shortcut: Shortcut
+	shortcut: Shortcut
+}
+
+export interface CreatingUser {
+	id?: string
+	email: string | null | undefined
+	name: string | null | undefined
+	posts?: Shortcut[]
 }
