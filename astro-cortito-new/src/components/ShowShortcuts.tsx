5a8cbed8f285dc958b9c03bbb0d4caa262aaa94f
Jose Miguel <josemiggomez16@gmail.com>
2024-04-07 16:21:55 -0400

    add loading while fetching shortcuts

--- astro-cortito-new/src/components/ShowShortcuts.tsx
+++ astro-cortito-new/src/components/ShowShortcuts.tsx
@@ -1,46 +1,62 @@
-import { useEffect, useState } from 'react'
+import type { Shortcut } from '../types'
 import type { Session } from '@auth/core/types'
+import { viewStore } from '../stores/viewStore'
+import { createdStore } from '../stores/createdStore'
+import { useEffect, useState } from 'react'
 import { useStore } from '@nanostores/react'
 import getUserShortcuts from '../libs/getUserShortcuts'
-import type { Shortcut } from '../types'
-import { viewStore } from '../stores/viewStore.js'
 import Card from './Card.jsx'
 import ListItem from './ListItem.jsx'
+import Loading from '../components/svg/Loading'
 
 interface Props {
 	session: Session | null
 }
 
 function ShowShortcuts ({ session }: Props) {
+	const [loading, setLoading] = useState(false)
 	const [shortcuts, setShortcuts] = useState([] as Shortcut[])
 	const view = useStore(viewStore)
+	const created = useStore(createdStore)
 
 	useEffect(() => {
 		const getUser = async () => {
 			if (session) {
+				setLoading(true)
 				const userShortcuts = await getUserShortcuts(session?.user?.email)
 				if (userShortcuts !== undefined) {
 					setShortcuts(userShortcuts)
 				}
+				setLoading(false)
 			}
 		}
 		getUser()
-	}, [])
+	}, [created])
 
 	return (
 		<>
-			{view === 'grid' && (
-				<main className='grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
-					{shortcuts &&
-						shortcuts?.map((shortcut) => <Card key={shortcut.id} shortcut={shortcut} />)}
-				</main>
-			)}
-			{view === 'list' && (
-				<main className='mt-6 overflow-hidden rounded'>
-					{shortcuts &&
-						shortcuts?.map((shortcut) => <ListItem key={shortcut.id} shortcut={shortcut} />)}
-				</main>
-			)}
+			{loading
+				? (
+					<div className='h-full grid place-content-center'>
+						<Loading className='w-10 mb-40 animate-spin' />
+					</div>
+				)
+				: (
+					<>
+						{view === 'grid' && (
+							<main className='grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+								{shortcuts &&
+								shortcuts?.map((shortcut) => <Card key={shortcut.id} shortcut={shortcut} />)}
+							</main>
+						)}
+						{view === 'list' && (
+							<main className='mt-6 overflow-hidden rounded flex flex-col gap-1'>
+								{shortcuts &&
+								shortcuts?.map((shortcut) => <ListItem key={shortcut.id} shortcut={shortcut} />)}
+							</main>
+						)}
+					</>
+				)}
 		</>
 	)
 }
