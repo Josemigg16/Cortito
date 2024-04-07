7d9da08e92cb0ce3da6b5c422f2373c93d60576f
Jose Miguel <josemiggomez16@gmail.com>
2024-04-07 16:19:18 -0400

    create listItem component

--- astro-cortito-new/src/components/ListItem.tsx
+++ astro-cortito-new/src/components/ListItem.tsx
@@ -1,26 +1,27 @@
-import useShortcutItem from '../hooks/useShortcutItem'
 import type { ShortcutAsProp } from '../types'
 import RowField from './RowField'
+import { makeURL } from '../helpers/makeURL'
+import { handleEdit } from '../helpers/handleEdit'
 
 function ListItem ({ shortcut }: ShortcutAsProp) {
-	const { title, description, oldLink, handleEdit, makeURL } =
-    useShortcutItem(shortcut)
-
 	return (
-		<button
-			onClick={handleEdit}
-			id='item'
-			className='relative grid h-14 cursor-pointer select-none grid-cols-2 place-content-center gap-2 bg-gray-800 pr-2 text-xl text-white transition-[background-color] hover:bg-gray-700 md:grid-cols-4 w-full'
-		>
-			<RowField info={title} className='uppercase' />
-			<RowField info={description} className='hidden md:block' />
-			<RowField info={oldLink} />
-			<RowField
-				info={makeURL(shortcut.newLink)}
-				isLink={true}
-				className='hidden md:block'
-			/>
-		</button>
+		<article>
+			<button
+				onMouseUp={(e: unknown) => handleEdit({ shortcut, e: e as MouseEvent })}
+				className='item relative grid h-14 cursor-pointer select-none grid-cols-2 place-content-center gap-2 bg-gray-900 pr-2 text-xl text-white transition-[background-color_.1s] hover:bg-gray-700 md:grid-cols-4 w-full rounded animate-fromOpacity'
+			>
+				<RowField
+					info={shortcut?.title || 'Untitled'}
+					className='uppercase text-2xl font-semibold'
+				/>
+				<RowField
+					info={shortcut?.description || 'There is no description.'}
+					className='hidden md:block'
+				/>
+				<RowField info={shortcut?.oldLink} />
+				<RowField info={makeURL(shortcut.newLink)} isLink={true} className='hidden md:block' />
+			</button>
+		</article>
 	)
 }
 
