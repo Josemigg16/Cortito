63c78d7c32c545ef9edd16487810f9e1e52dc428
Jose Miguel <josemiggomez16@gmail.com>
2024-04-07 16:20:12 -0400

    style rowfield

--- astro-cortito-new/src/components/RowField.tsx
+++ astro-cortito-new/src/components/RowField.tsx
@@ -1,24 +1,27 @@
+import { modalStore } from '../stores/modalStore'
+
 interface Props {
-  info: string
-  className?: string
-  isLink?: boolean
+	info: string
+	className?: string
+	isLink?: boolean
 }
 
 function RowInput ({ info, className, isLink }: Props) {
 	return !isLink
 		? (
 			<div
-				className={`inline-block h-full overflow-hidden text-ellipsis text-nowrap bg-transparent focus-within:bg-gray-900 ${className} pl-6 pt-2`}
+				className={`inline-block h-full text-ellipsis text-nowrap bg-transparent focus-within:bg-gray-900 pt-2 ${className}`}
 			>
-				{info}
+				<p className='text-start text-ellipsis overflow-hidden pl-8'>{info}</p>
 			</div>
 		)
 		: (
 			<a
 				tabIndex={-1}
-				className={`overflow-hidden text-ellipsis bg-transparent pt-2 hover:underline ${className}`}
+				className={`overflow-hidden text-ellipsis bg-transparent pt-2 hover:underline z-20 ${className}`}
 				target='_blank'
 				href={info}
+				onMouseUp={() => setTimeout(() => modalStore.set(false), 1)}
 			>
 				{info}
 			</a>
